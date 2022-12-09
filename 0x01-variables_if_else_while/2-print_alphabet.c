8d0a810127015f69de384fc8b65d4d8f56011865
asmaadrahim <asmaaham96@gmail.com>
2022-12-09 09:46:40 -0800

    2_TASK

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,13 @@
 */
 int main(void)
 {
-	char c;
-
-	c = 'a';
-       while
-	       (c <= 'z') {
-			putchar(c);
-       			c++;
-		}
-       putchar('\n');
-	return (0);
+char c;
+c = 'a';
+while
+(c <= 'z') {
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
 }
