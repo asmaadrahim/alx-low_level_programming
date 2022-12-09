02bda49b8ee51cdd9d414ec5df2880f455ba93f1
asmaadrahim <asmaaham96@gmail.com>
2022-12-09 10:02:32 -0800

    8_TASK

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 58; i++)
+	for (i = 48; i < 58; i++)
 		putchar(i);
 	for (i = 'a'; i <= 'f'; i++)
 		putchar(i);
-		putchar('\n');
-
+	putchar('\n');
 	return (0);
 }
