ec8f74a7bb8e4990b5aba9d9599df3d85952db2c
asmaadrahim <asmaaham96@gmail.com>
2023-01-02 15:32:39 -0800

    MSG

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * _memcpy - function
+ *
+ * @src: pointer
+ * @dest: pointer
+ * @n: counter
+ * Return: (s)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int counter;
+
+	for (counter = 0; counter < n; counter++)
+	{
+		dest[counter] = src[counter];
+	}
+	return (dest);
+}
