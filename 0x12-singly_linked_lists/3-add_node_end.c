b3262c63ff76325a1e26cce7cace1eec95a0715d
asmaadrahim <asmaaham96@gmail.com>
2023-01-26 21:34:49 +0200

    ALLDONE

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: double pointer to the list_t list
+ * @str: string to put in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+ list_t *nwere;
+ list_t *temptemp = *head;
+ unsigned int len = 0;
+
+ while (str[len])
+ len++;
+
+ nwere = malloc(sizeof(list_t));
+ if (!nwere)
+ return (NULL);
+
+ nwere->str = strdup(str);
+ nwere->len = len;
+ nwere->next = NULL;
+
+ if (*head == NULL)
+ {
+ *head = nwere;
+ return (nwere);
+ }
+
+ while (temptemp->next)
+ temptemp = temptemp->next;
+
+ temptemp->next = nwere;
+
+ return (nwere);
+}
+
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
+void free_list(list_t *head)
+{
+ list_t *temptemp;
+
+ while (head)
+ {
+ temptemp = head->next;
+ free(head->str);
+ free(head);
+ head = temptemp;
+ }
+}
+
